a3c078dd279e0b1c523c355c5e9fe0e15a74f55e
somendra sonar <somendrasonar24@gmail.com>
2026-03-22 03:08:15 +0530

    3

--- 1_twosum.java
+++ 1_twosum.java
@@ -4,40 +4,26 @@ import java.util.*;
 
 //using sorting o(nlogn)
 class 1_twosum {
-    public int[] twoSum(int[] nums, int target) {
+     public static int[] twoSum(int[] A, int target) {
+       // Your code here
+       Arrays.sort(A);
+       int left = 0;
+       int right = A.length-1;
+       int ans[] = {-1,-1};
 
-        int n = nums.length;
+       while(left<right){
+        int sum = A[left]+A[right];
 
-        int[][] arr = new int[n][2];
-
-        for(int i = 0; i < n; i++){
-            arr[i][0] = nums[i];   // value
-            arr[i][1] = i;         // original index
+        if(sum== target){
+            ans[0] = left+1;
+            ans[1] = right+1;
+            return ans;
         }
-
-        Arrays.sort(arr, (a,b) -> a[0] - b[0]);
-
-        int start = 0;
-        int end = n - 1;
-
-        while(start < end){
-
-            int sum = arr[start][0] + arr[end][0];
-
-            if(sum == target){
-                return new int[]{arr[start][1], arr[end][1]};
-            }
-            else if(sum < target){
-                start++;
-            }
-            else{
-                end--;
-            }
-
-        }
-
-        return new int[]{-1,-1};
-    }
+        else if(sum<target) left++;
+        else right++;
+       }
+       return ans;
+}
 }
 
 
---
+++ trail.java
@@ -0,0 +1,8 @@
+public class trail {
+    public static void main(String[] args) {
+        int a =8;
+        int b = 8;
+        System.out.println((a/3)*3+(b/3));
+    }
+
+}
